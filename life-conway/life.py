e69590c2a040e164ee9a446a6f9e907a6284791d
PaulAustin <canoetime@gmail.com>
2020-04-17 02:12:36 -0500

    added some key and mouse events
    other refactoring

--- life-conway/life.py
+++ life-conway/life.py
@@ -9,17 +9,23 @@ WIDTH = (COLS * CELL_SIZE)
 BACK_COLOR = 255, 255, 255
 CELL_COLOR = (0, 200, 0)
 
-def init_lives(rows, cols):
-    grid = []
-    for r in range(rows):
-        row = []
-        for c in range(cols):
-            # Build the columns by appending cells to each row
-            row.append(random.randint(0, 7) == 0)
+g_changed = False
+g_delay = 0
+g_tics  = 0
+g_paused = False
 
-        # Build the world one row at a time.
-        grid.append(row)
-    return grid
+def grid_clear(grid):
+    for r in range(len(grid)):
+        for c in range(len(grid[0])):
+            grid[r][c] = False
+
+def grid_random(grid):
+    for r in range(len(grid)):
+        for c in range(len(grid[0])):
+            grid[r][c] = random.randint(0, 7) == 0
+
+def grid_build(rows, cols):
+    return [[False for c in range(cols)] for r in range(rows)]
 
 def draw_cell(r, c):
     cx = CELL_SIZE * c
@@ -29,6 +35,11 @@ def draw_cell(r, c):
     return;
 
 def draw():
+    global g_changed
+    if not g_changed:
+        return
+    g_changed = False
+
     screen.clear()
     for r in range(ROWS):
         for c in range(COLS):
@@ -43,12 +54,16 @@ def count_neighbors(w, r, c):
         for nc in range(max(c-1,0), min(c+1,COLS-1) + 1):
             if w[nr][nc]:
                 sum += 1
+    # Loop above added the center cell, subtract it back out.
     if w[r][c]:
         sum -= 1
     return sum
 
 def update():
-    global worldNext, world
+    global g_changed
+    if g_paused:
+        return
+    g_changed = True
 
     # Calculate the next state
     for r in range(ROWS):
@@ -66,12 +81,25 @@ def update():
         for c in range(COLS):
             world[r][c] = worldNext[r][c]
 
-
 def on_mouse_down(pos, button):
-    print (" on mouse down")
+    global g_changed
+    r = pos[1] // CELL_SIZE
+    c = pos[0] // CELL_SIZE
+    world[r][c] = not world[r][c]
+    g_changed = True
 
-world = init_lives(ROWS, COLS)
-worldNext = init_lives(ROWS, COLS)
+def on_key_down(key,mod,unicode):
+    global g_paused
+    if (key == keys.SPACE):
+        g_paused = not g_paused
+    if (key == keys.C):
+        grid_clear(world)
+    if (key == keys.R):
+        grid_random(world)
+
+world = grid_build(ROWS, COLS)
+grid_random(world)
+worldNext = grid_build(ROWS, COLS)
 
 
 
