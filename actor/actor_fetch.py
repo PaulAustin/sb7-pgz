ee6e1ef75bfb0338ea9a0386be85bcade376179f
PaulAustin <canoetime@gmail.com>
2020-04-23 14:35:58 -0500

    actor_fatch - two concurrent animations

---
+++ actor/actor_fetch.py
@@ -0,0 +1,39 @@
+# Example using pygame-zero actor object
+# move the ball to where the mouse is clicked
+# with the pgz animation function
+
+BACK_COLOR = (200, 225, 255)
+HEIGHT = 300
+WIDTH = 500
+BALL = 'ball_100x100'
+DOG2 = 'dog2_100x100'
+
+ball = Actor(BALL, (100, 100))
+dog = Actor(DOG2, (100, 100))
+
+def draw():
+    screen.fill(BACK_COLOR)
+    dog.draw()
+    ball.draw()
+
+def on_mouse_down(pos, button):
+    # base animation time on distance
+    # 1000 pixels per second
+    d = ball.distance_to(pos)
+
+    if d != 0:
+        dog.angle = dog.angle_to(pos) + 90
+
+        # Throw the ball
+        animate(ball,
+            tween= 'decelerate',
+            duration= d / 5000,
+            on_finished= (lambda: sounds.pop.play()),
+            pos= pos)
+
+        # Start dog running at 1/4 the speed
+        animate(dog,
+            tween= 'decelerate',
+            duration= d / 500,
+            on_finished= (lambda: sounds.bark.play()),
+            pos= pos)
