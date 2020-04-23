d67ba421324f3c8c5a457fb88a6a3828776db3b9
PaulAustin <canoetime@gmail.com>
2020-04-23 10:32:17 -0500

    Added a flip

--- actor/actor_jump.py
+++ actor/actor_jump.py
@@ -23,21 +23,36 @@ class Game:
 
     def on_update(self):
         if keyboard.space and self.ani_obj == None:
+            # Start the jump by moving up
+            # slowing as it gets to the peak
             self.jumping = True
             self.ani_obj = animate(self.dog,
                 tween='decelerate',
                 duration = JUMP_T,
                 on_finished = animation_done,
                 pos = (200, GROUND_Y - JUMP_Y))
+
+            # A second animation does a flip
+            # This one is set to be twice as long
+            # The the jump is split into two parts
+            animate(self.dog,
+                tween='linear',
+                duration = JUMP_T * 2,
+                on_finished = None,
+                angle = -360)
+
             sounds.bark.play()
 
     def on_draw(self):
         screen.fill(BACK_COLOR)
+        screen.draw.text("Angle = " + str(int(self.dog.angle)),(20,20))
         self.dog.draw()
 
     def on_ani_done(self):
         self.ani_obj = None
         if self.jumping:
+            # Finish the jump by moving down
+            # starting slowing the speeding up
             self.ani_obj = animate(self.dog,
                 tween='accelerate',
                 duration = JUMP_T,
@@ -45,6 +60,8 @@ class Game:
                 pos = (200, GROUND_Y))
             self.jumping = False
         else:
+            # Reset the angle so next fil will work
+            self.dog.angle = 0
             sounds.pop.play()
 
 
